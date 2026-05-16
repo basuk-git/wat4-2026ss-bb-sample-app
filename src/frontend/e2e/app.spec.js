34c25aa4b6b7884665d75aab291c37a70cb141c5
basuk-git <lukstar@gmx.at>
2026-05-16 13:49:14 +0200

    fix

--- src/frontend/e2e/app.spec.js
+++ src/frontend/e2e/app.spec.js
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
+  await page.goto('/');
   await page.getByRole('button', { name: 'Increment' }).click();
   await expect(page.getByTestId('counter-value')).toContainText('1');
 });
